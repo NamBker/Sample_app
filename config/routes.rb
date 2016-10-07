1c210b7280a62d5bd59b1ead790c80930e34d154
NamBker <namvn@runsystem.net>
2016-10-08 05:13:22 +0700

    finish login-out

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   # get 'static_pages/home'
-  get 'help' => 'static_pages#help'
-  get 'signup' => 'users#new'
-  post 'signup' => 'users#create'
+  get 'help' 	   => 'static_pages#help'
+  get 'signup'     => 'users#new'
+  post 'signup'    => 'users#create'
+  get 'login'  => 'sessions#new'
+  post 'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
-  get 'static_pages/home'
-  get 'static_pages/help'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file see http://guides.rubyonrails.org/routing.html
 end
