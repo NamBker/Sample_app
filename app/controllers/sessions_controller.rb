34ed4b96b5e7c37b73ea522a763414b94905c988
NamBker <namvn@runsystem.net>
2016-10-13 11:50:28 +0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      flash[:success] = "Welcome to the Sample App!"
       remember user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
